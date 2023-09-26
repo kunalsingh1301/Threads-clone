e19c555787acc8553f5140a248318b5905698de3
kunalsingh1301 <kunal.singh@iitg.ac.in>
2023-09-26 23:55:04 +0530

    D-1

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,27 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import {Inter} from "next/font/google";
+import '../globals.css';
+
+// SEO Optimization
+export const metadata = {
+    title:'Threads',
+    description:'A next.js threads-clone'
+}
+
+const inter = Inter({subsets:["latin"]})
+
+export default function Rootlayout({
+    children
+}:{
+    children:React.ReactNode
+}){
+    return (
+        <ClerkProvider>
+            <html lang='en'>
+                <body className={`${inter.className} bg-dark-1`}>
+                    {children}
+                </body>
+            </html>
+        </ClerkProvider>
+    )
+}
