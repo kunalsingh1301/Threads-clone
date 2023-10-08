c1a690126c8d1abb92b6a8fdba6f4b232077c8b4
kunalsingh1301 <kunal.singh@iitg.ac.in>
2023-10-08 15:31:53 +0530

    8 oct

--- components/shared/Leftsidebar.tsx
+++ components/shared/Leftsidebar.tsx
@@ -3,18 +3,21 @@ import { sidebarLinks } from "@/constants"
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
-import { SignOutButton, SignedIn } from "@clerk/nextjs";
+import { SignOutButton, SignedIn,useAuth } from "@clerk/nextjs";
 
 function Leftsidebar() {
     // console.log("sidebarLinks:", sidebarLinks)
     const router = useRouter();
     const pathname = usePathname();
+    const {userId} = useAuth();
     return (
         <section className="custom-scrollbar leftsidebar">
             <div className="flex w-full flex-1 flex-col gap-6 px-6">
-                {sidebarLinks && sidebarLinks.map((link) => {
+                { sidebarLinks?.map((link) => {
 
-                    const isActive = (pathname.includes(link.route) && link.route.length > 1) || pathname === link.route
+                    const isActive = (pathname.includes(link.route) && link.route.length > 1) || pathname === link.route;
+
+                    if(link.route === '/profile') link.route = `${link.route}/${userId}`
 
                     return (
                         <Link
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,8 +1,10 @@
+
 "use server"
 
 import { revalidatePath } from "next/cache";
 import User from "../models/user.model";
 import { connectToDB } from "../mongoose"
+import Thread from '../models/thread.model';
 
 interface Params{
     userId:string;
@@ -49,5 +51,32 @@ export async function fetchUser(userId:string){
         return await User.findOne({id:userId})
     } catch (error:any) {
         throw new Error(`Failed to create/update user:${error.message}`)
+    }
+}
+
+export async function fetchUserPosts(userid:string) {
+    try {
+        connectToDB();
+
+        //Find all threads created by user
+
+        //TODO: Populate community
+        const threads = await User.findOne({id:userid}).populate({
+            path:'threads',
+            model:Thread,
+            populate:{
+                path:'children',
+                model:Thread,
+                populate:{
+                    path:'author',
+                    model:User,
+                    select:'name image id'
+                }
+            }
+        })
+
+        return threads
+    } catch (error:any) {
+        throw new Error(`Failed to fetch user posts:${error.message}`)
     }
 }
